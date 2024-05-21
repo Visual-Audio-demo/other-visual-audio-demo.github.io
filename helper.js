37848268a5c820cc72f354bc2ca91c4172215a5b
Your Name <you@example.com>
2024-05-22 00:31:21 +0800

    第三次提交

--- helper.js
+++ helper.js
@@ -20,6 +20,21 @@ function generateExampleRow(table_row, base_path, filename_ext, col_offset) {
     }
   }
 }
+function generateExampleRowNew(table_row, base_path, filename_ext, models, col_offset) {
+  for (var i = 0; i < models.length; i++) {
+    let p = base_path + models[i] + '/' + filename_ext;
+    let cell = table_row.cells[col_offset + i];
+    // console.log(table_row.cells.length)
+    if (p.endsWith('txt')) {
+      var req = new XMLHttpRequest();
+      req.open("GET", p, false);
+      req.send(null);
+      cell.innerHTML = '<font size="-1">' + req.responseText + '</font>';
+    } else {
+      cell.innerHTML = cell.innerHTML + createAudioHTML(p);
+    }
+  }
+}
 
 function generateExampleRowCross(table_row, base_path, filename_ext, col_offset) {
   for (var i = 0; i < filename_ext.length; i++) {
@@ -43,15 +58,11 @@ function generateExampleRowCross(table_row, base_path, filename_ext, col_offset)
 
 function generateTable1Vggsound(tableId) {  
   let table = document.getElementById(tableId);  
-  let ext = ['_gt.wav', '_emotion.wav', '_style.wav', '_speed.wav','_energy.wav','_semantic.wav'];  
-  let ext1 = ['_gt.txt','_emotion.txt','_style.txt','_speed.txt','_energy.txt','_semantic.txt']  
+  let name = ['_SrU3mfTPYg_000032.wav', '_wNC03n2xmg_000211.wav', 'jJ6FWcy7B6M_000052.wav', 'RpGRpTpEHTs_000109.wav', 'ZGYQX-172io_000022.wav']
+  let models = ['gt', 'gt_voc', 'diff-foley', 'ours'];  
   
-  for (var i = 0; i < 4; i++) {  
-    if (i % 2 === 0) {  
-      generateExampleRow(table.rows[1 + i], 'data/pipeline/' + i, ext, 0);  
-    } else {  
-      generateExampleRow(table.rows[1 + i], 'data/pipeline/' + i, ext1, 0);  
-    }  
+  for (var i = 0; i < 5; i++) {  
+    generateExampleRowNew(table.rows[1 + i], 'data/table1_vggsound/', name[i], models, 0);  
   } 
 } 
 
@@ -112,9 +123,9 @@ function generateTable2Landscape(tableId) {
 } 
 
 generateTable1Vggsound('vggsound1-table')
-generateTable1Landscape('table1-landscape')
-generateTable2AIST('table2-aist')
-generateTable2YT8M('table2-yt8m')
-generateTable2Landscape('table2-landscape')
+generateTable1Landscape('landscape1-table')
+generateTable2AIST('aist2-table')
+generateTable2YT8M('yt8m2-table')
+generateTable2Landscape('landscape2-table')
 
 
