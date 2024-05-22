90fa10ccc26d4ba6a24113df9760cfced466c0a2
Your Name <you@example.com>
2024-05-22 09:57:41 +0800

    第九次更新

--- helper.js
+++ helper.js
@@ -71,7 +71,7 @@ function generateTable1Landscape(tableId) {
   let name = ['1Ef9CAZ3OrQ_9993_10003.wav', '1i79N_6wuIA_3290_3300.wav', '2dkilldNRJE_3717_3727.wav', 'g_2ioOlo0u5I_19826_19836.wav','MZjZEVgQmno_21_31.wav'];  
   let models = ['gt', 'gt_voc', 'diff-foley','mmdiffusion', 'ours']  
   
-  for (var i = 0; i < 4; i++) {  
+  for (var i = 0; i < 5; i++) {  
     generateExampleRowNew(table.rows[1 + i], 'data/table1_landscape/', name[i], models, 0);  
   } 
 } 
@@ -81,7 +81,7 @@ function generateTable2AIST(tableId) {
   let name = ['gBR_sBM_c01_d04_mBR0_ch02.wav', 'gHO_sBM_c01_d21_mHO5_ch02.wav', 'gJB_sBM_c01_d08_mJB5_ch02.wav', 'gJB_sBM_c01_d09_mJB5_ch02.wav','gKR_sBM_c01_d30_mKR2_ch02.wav'];  
   let models = ['gt', 'gt_voc', 'lora','all', 'bs'] 
   
-  for (var i = 0; i < 4; i++) {  
+  for (var i = 0; i < 5; i++) {  
     generateExampleRowNew(table.rows[1 + i], 'data/table2_aist/', name[i], models, 0);  
   } 
 } 
@@ -91,7 +91,7 @@ function generateTable2YT8M(tableId) {
   let name = ['y-0m6sVZ7kss.wav', 'y-1XvXK28_So.wav', 'y-L2prOTjIPk.wav', 'y-mwMtk9CMxs.wav','y-Qni8ZE4Y8I.wav'];  
   let models = ['gt', 'lora','all', 'bs'] 
   
-  for (var i = 0; i < 4; i++) {  
+  for (var i = 0; i < 5; i++) {  
     generateExampleRowNew(table.rows[1 + i], 'data/table2_yt8m/', name[i], models, 0);
   } 
 } 
@@ -101,7 +101,7 @@ function generateTable2Landscape(tableId) {
   let name = ['1Ef9CAZ3OrQ_9993_10003_0.wav', '1i79N_6wuIA_3290_3300_0.wav', 'g_2ioOlo0u5I_19826_19836_0.wav', 'g-WrD5DwI6_4_8410_8420_0.wav','MZjZEVgQmno_21_31_0.wav'];  
   let models = ['lora','all', 'bs'] 
   
-  for (var i = 0; i < 4; i++) {  
+  for (var i = 0; i < 5; i++) {  
     generateExampleRowNew(table.rows[1 + i], 'data/table2_landscape/', name[i], models, 0);
   } 
 } 
