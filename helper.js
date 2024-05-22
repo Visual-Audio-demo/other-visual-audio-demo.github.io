0365cf103ae29531bd3c91457644e6bc4c16216f
Your Name <you@example.com>
2024-05-22 16:26:58 +0800

    add video

--- helper.js
+++ helper.js
@@ -3,29 +3,46 @@ function createAudioHTML(path) {
       path +
       ' type="audio/wav">Your browser does not support the audio element.</audio>';
 }
-
+function createVideoHTML(path) {  
+  return '<video controls class="px-1" width="320" height="240"> <source src="' +  
+      path +  
+      '" type="video/mp4">Your browser does not support the video tag.</video>';  
+}
 function generateExampleRowNew(table_row, base_path, filename_ext, models, col_offset) {
   for (var i = 0; i < models.length; i++) {
-    let p = base_path + models[i] + '/' + filename_ext;
-    let cell = table_row.cells[col_offset + i];
-    // console.log(table_row.cells.length)
-    if (p.endsWith('txt')) {
-      var req = new XMLHttpRequest();
-      req.open("GET", p, false);
-      req.send(null);
-      cell.innerHTML = '<font size="-1">' + req.responseText + '</font>';
-    } else {
-      cell.innerHTML = cell.innerHTML + createAudioHTML(p);
-    }
+    let p = base_path + models[i] + '/' + filename_ext;  
+    let cell = table_row.cells[col_offset + i];  
+  
+    if (p.endsWith('.txt')) {  
+      // 使用同步XMLHttpRequest（不推荐，但为了示例保持一致）  
+      var req = new XMLHttpRequest();  
+      req.open("GET", p, false);  
+      req.send(null);  
+      cell.innerHTML = '<font size="-1">' + req.responseText + '</font>';  
+    } else if (p.endsWith('.wav') || p.endsWith('.mp3')) { // 假设音频文件是wav或mp3格式  
+      cell.innerHTML = cell.innerHTML + createAudioHTML(p);  
+    } else if (p.endsWith('.mp4')) { // 假设视频文件是mp4格式  
+      cell.innerHTML = cell.innerHTML + createVideoHTML(p);  
+    }  
+  }
+}
+function generateExampleRowNewVideo(table_row, base_path, filename_ext, models, col_offset) {
+  let p = base_path + models[0] + '/' + filename_ext + '.mp4';  
+  let cell = table_row.cells[col_offset + 0]; 
+  cell.innerHTML = cell.innerHTML + createVideoHTML(p);
+  for (var i = 1; i < models.length; i++) {
+    let p = base_path + models[i] + '/' + filename_ext + '.wav';  
+    let cell = table_row.cells[col_offset + i];  
+    cell.innerHTML = cell.innerHTML + createAudioHTML(p);  
   }
 }
 function generateTable1Vggsound(tableId) {  
   let table = document.getElementById(tableId);  
-  let name = ['gSrU3mfTPYg_000032.wav', 'gwNC03n2xmg_000211.wav', 'jJ6FWcy7B6M_000052.wav', 'RpGRpTpEHTs_000109.wav', 'ZGYQX-172io_000022.wav']
-  let models = ['gt', 'gt_voc', 'diff-foley', 'ours'];  
+  let name = ['gSrU3mfTPYg_000032', 'gwNC03n2xmg_000211', 'jJ6FWcy7B6M_000052', 'RpGRpTpEHTs_000109', 'ZGYQX-172io_000022']
+  let models = ['video', 'gt', 'gt_voc', 'diff-foley', 'ours'];  
   
   for (var i = 0; i < 5; i++) {  
-    generateExampleRowNew(table.rows[1 + i], 'data/table1_vggsound/', name[i], models, 0);  
+    generateExampleRowNewVideo(table.rows[1 + i], 'data/table1_vggsound/', name[i], models, 0);  
   } 
 } 
 
