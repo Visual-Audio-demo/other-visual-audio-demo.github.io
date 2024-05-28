869b655ca41150b26cf02a59f8d33e6dea1c1a07
Your Name <you@example.com>
2024-05-28 17:42:25 +0800

    all video

--- helper.js
+++ helper.js
@@ -18,53 +18,60 @@ function generateExampleRowNewVideo(table_row, base_path, filename_ext, models, 
     cell.innerHTML = cell.innerHTML + createAudioHTML(p);  
   }
 }
+function generateExampleRowVideo(table_row, base_path, filename_ext, models, col_offset) {
+  for (var i = 1; i < models.length; i++) {
+    let p = base_path + models[i] + '/' + filename_ext + '.mp4'; 
+    let cell = table_row.cells[col_offset + i];  
+    cell.innerHTML = cell.innerHTML + createVideoHTML(p); 
+  }
+}
 function generateTable1Vggsound(tableId) {  
   let table = document.getElementById(tableId);  
   let name = ['gSrU3mfTPYg_000032', 'gwNC03n2xmg_000211', 'jJ6FWcy7B6M_000052', 'RpGRpTpEHTs_000109', 'ZGYQX-172io_000022']
-  let models = ['video', 'gt', 'gt_voc', 'diff-foley', 'ours'];  
+  let models = ['gt', 'gt_voc', 'diff-foley', 'ours'];  
   
   for (var i = 0; i < 5; i++) {  
-    generateExampleRowNewVideo(table.rows[1 + i], 'data/table1_vggsound/', name[i], models, 0);  
+    generateExampleRowVideo(table.rows[1 + i], 'data/table1_vggsound/', name[i], models, 0);  
   } 
 } 
 
 function generateTable1Landscape(tableId) {  
   let table = document.getElementById(tableId);  
   let name = ['1Ef9CAZ3OrQ_9993_10003', '1i79N_6wuIA_3290_3300', '2dkilldNRJE_3717_3727', 'g_2ioOlo0u5I_19826_19836','MZjZEVgQmno_21_31'];  
-  let models = ['video', 'gt', 'gt_voc', 'diff-foley','mmdiffusion', 'ours']  
+  let models = ['gt', 'gt_voc', 'diff-foley','mmdiffusion', 'ours']  
   
   for (var i = 0; i < 5; i++) {  
-    generateExampleRowNewVideo(table.rows[1 + i], 'data/table1_landscape/', name[i], models, 0);  
+    generateExampleRowVideo(table.rows[1 + i], 'data/table1_landscape/', name[i], models, 0);  
   } 
 } 
 
 function generateTable2AIST(tableId) {  
   let table = document.getElementById(tableId);  
   let name = ['gBR_sBM_c01_d04_mBR0_ch02', 'gHO_sBM_c01_d21_mHO5_ch02', 'gJB_sBM_c01_d08_mJB5_ch02', 'gJB_sBM_c01_d09_mJB5_ch02','gKR_sBM_c01_d30_mKR2_ch02'];  
-  let models = ['video', 'gt', 'gt_voc', 'lora','all', 'bs'] 
+  let models = ['gt', 'gt_voc', 'lora','all', 'bs'] 
   
   for (var i = 0; i < 5; i++) {  
-    generateExampleRowNewVideo(table.rows[1 + i], 'data/table2_aist/', name[i], models, 0);  
+    generateExampleRowVideo(table.rows[1 + i], 'data/table2_aist/', name[i], models, 0);  
   } 
 } 
 
 function generateTable2YT8M(tableId) {  
   let table = document.getElementById(tableId);  
   let name = ['y-0m6sVZ7kss', 'y-1XvXK28_So', 'y-L2prOTjIPk', 'y-mwMtk9CMxs','y-Qni8ZE4Y8I'];  
-  let models = ['video', 'gt', 'lora','all', 'bs'] 
+  let models = ['gt', 'lora','all', 'bs'] 
   
   for (var i = 0; i < 5; i++) {  
-    generateExampleRowNewVideo(table.rows[1 + i], 'data/table2_yt8m/', name[i], models, 0);
+    generateExampleRowVideo(table.rows[1 + i], 'data/table2_yt8m/', name[i], models, 0);
   } 
 } 
 
 function generateTable2Landscape(tableId) {  
   let table = document.getElementById(tableId);  
   let name = ['1Ef9CAZ3OrQ_9993_10003_0', '1i79N_6wuIA_3290_3300_0', 'g_2ioOlo0u5I_19826_19836_0', 'g-WrD5DwI6_4_8410_8420_0','MZjZEVgQmno_21_31_0'];  
-  let models = ['video', 'lora','all', 'bs'] 
+  let models = ['lora','all', 'bs'] 
   
   for (var i = 0; i < 5; i++) {  
-    generateExampleRowNewVideo(table.rows[1 + i], 'data/table2_landscape/', name[i], models, 0);
+    generateExampleRowVideo(table.rows[1 + i], 'data/table2_landscape/', name[i], models, 0);
   } 
 } 
 
